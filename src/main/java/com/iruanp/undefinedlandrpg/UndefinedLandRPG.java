41ba4cbd67222a85a8b5d9e60692d6126dc538e3
一张软盘 <68799904+ifloppy@users.noreply.github.com>
2023-08-29 19:44:05 +0800

    Create

---
+++ src/main/java/com/iruanp/undefinedlandrpg/UndefinedLandRPG.java
@@ -0,0 +1,17 @@
+package com.iruanp.undefinedlandrpg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UndefinedLandRPG extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
